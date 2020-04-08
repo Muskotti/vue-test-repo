99464d3145efe2eb3219848c1be408811c71f02b
Toni Vänttinen <Toni.vanttinen@dnainternet.net>
2020-04-08 15:04:34 +0300

    form basic validation

--- src/components/MyForm.vue
+++ src/components/MyForm.vue
@@ -3,28 +3,58 @@
     <v-card-text>
       <p class="display-1 text--primary">Enter information</p>
     </v-card-text>
-    <v-form>
+    <v-form ref="form" v-model="valid">
       <v-container>
         <v-row>
           <v-col cols="12" md="4">
-            <v-text-field label="First name" required></v-text-field>
+            <v-text-field
+              v-model="fname"
+              label="First name"
+              :rules="[v => !!v || 'Enter first name']"
+              required
+            ></v-text-field>
           </v-col>
 
           <v-col cols="12" md="4">
-            <v-text-field label="Last name" required></v-text-field>
+            <v-text-field
+              v-model="lname"
+              label="Last name"
+              :rules="[v => !!v || 'Enter last name']"
+              required
+            ></v-text-field>
           </v-col>
 
           <v-col cols="12" md="4">
-            <v-text-field label="E-mail" required></v-text-field>
+            <v-text-field
+              v-model="email"
+              label="E-mail"
+              :rules="[v => !!v || 'Enter e-mail']"
+              required
+            ></v-text-field>
           </v-col>
         </v-row>
       </v-container>
     </v-form>
+    <v-card-actions>
+      <v-btn :disabled="!valid" color="success">Submit</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
 export default {
-  props: {}
+  data() {
+    return {
+      valid: false,
+      fname: "",
+      lname: "",
+      email: ""
+    };
+  },
+  methods: {
+    test() {
+      console.log(this.fname);
+    }
+  }
 };
 </script>
